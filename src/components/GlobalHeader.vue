cfacfc005a258e7eacffe356f806b5b2f97ba4b3
阿元 <jizuyuan@outlook.com>
2025-03-14 15:10:11 +0800

    vue3前端项目初始化
    vue3前端项目初始化

--- src/components/GlobalHeader.vue
+++ src/components/GlobalHeader.vue
@@ -1,39 +1,43 @@
 <template>
   <div id="globalHeader">
-
     <a-row :wrap="false">
-  <a-col flex="200px">
-    <RouterLink to="/">
-      <div class="title-bar">
-        <img class="logo" src="../assets/favicon.ico" alt="logo" />
-        <div class="title">ZN1</div>
-      </div>
-    </RouterLink>
-  </a-col>
-  <a-col flex="auto">
-    <a-menu
-      v-model:selectedKeys="current"
-      mode="horizontal"
-      :items="items"
-    />
-  </a-col>
-  <a-col flex="120px">
-    <div class="user-login-status">
-      <a-button type="primary" href="/user/login">登录</a-button>
-    </div>
-  </a-col>
-</a-row>
-
+      <a-col flex="200px">
+        <RouterLink to="/">
+          <div class="title-bar">
+            <img class="logo" src="../assets/favicon.ico" alt="logo" />
+            <div class="title">ZN1</div>
+          </div>
+        </RouterLink>
+      </a-col>
+      <a-col flex="auto">
+        <a-menu
+          v-model:selectedKeys="current"
+          mode="horizontal"
+          :items="items"
+          @click="doMenuClick"
+        />
+      </a-col>
+      <a-col flex="120px">
+        <div class="user-login-status">
+          <div v-if="loginUserStore.loginUser.id">
+            {{ loginUserStore.loginUser.userName ?? '无名' }}
+          </div>
+          <div v-else>
+            <a-button type="primary" href="/user/login">登录</a-button>
+          </div>
+        </div>
+      </a-col>
+    </a-row>
   </div>
 </template>
 <script lang="ts" setup>
-import { h, ref } from 'vue';
-import { MailOutlined, AppstoreOutlined, SettingOutlined } from '@ant-design/icons-vue';
-import { MenuProps } from 'ant-design-vue';
-const current = ref<string[]>(['home']);
+import { h, ref } from 'vue'
+import { MailOutlined, AppstoreOutlined, SettingOutlined } from '@ant-design/icons-vue'
+import { MenuProps } from 'ant-design-vue'
+
 const items = ref<MenuProps['items']>([
   {
-    key: 'home',
+    key: '/',
     icon: () => h(MailOutlined),
     label: '主页',
     title: '主页',
@@ -44,7 +48,35 @@ const items = ref<MenuProps['items']>([
     label: '关于',
     title: '关于',
   },
-]);
+])
+
+import { useRouter } from 'vue-router'
+const router = useRouter()
+
+//路由跳转事件
+const doMenuClick = ({ key }: { key: string }) => {
+  router.push({
+    path: key,
+  })
+}
+
+//当前选中菜单
+const current = ref<string[]>([])
+//监听路由变化，更新当前选中的菜单
+router.afterEach((to, from, next) => {
+  current.value = [to.path]
+})
+
+import { healthUsingGet } from '@/api/mainController'
+healthUsingGet().then((res) => {
+  console.log(res)
+})
+
+import { useLoginUserStore } from '@/stores/useLonginUserStore'
+
+const loginUserStore = useLoginUserStore()
+loginUserStore.fetchLoginUser()
+
 </script>
 
 <style scoped>
@@ -63,4 +95,3 @@ const items = ref<MenuProps['items']>([
   height: 48px;
 }
 </style>
-
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="HomePage">
+    <h1 class="green">You’ve successfully created a project with</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+#HomePage {
+
+}
+</style>
